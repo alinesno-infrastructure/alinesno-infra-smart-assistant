c12f8b1995e831f398c4d4f329fccc07c9683a62
CarrotSea233 <2733614757@qq.com>
2023-11-24 18:50:31 +0800

    AIGC

---
+++ alinesno-infra-smart-assistant-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/ThirdPartyAppEntity.java
@@ -0,0 +1,57 @@
+package com.alinesno.infra.smart.assistant.entity;
+
+import java.util.Date;
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+@Data
+@TableName("third_party_app")
+public class ThirdPartyAppEntity extends InfraBaseEntity {
+	/**
+	 * 应用ID
+	 */
+	@ColumnType(MySqlTypeConstant.BIGINT)
+	@ColumnComment("应用ID")
+	@TableField("app_id")
+	private Long appId;
+
+	/**
+	 * 应用名称
+	 */
+	@ColumnType(length = 50)
+	@ColumnComment("应用名称")
+	@TableField("app_name")
+	private String appName;
+
+	/**
+	 * 应用描述
+	 */
+	@ColumnType(MySqlTypeConstant.TEXT)
+	@ColumnComment("应用描述")
+	@TableField("app_description")
+	private String appDescription;
+
+	/**
+	 * 接口地址
+	 */
+	@ColumnType(length = 100)
+	@ColumnComment("接口地址")
+	@TableField("api_url")
+	private String apiUrl;
+
+
+}
---
+++ alinesno-infra-smart-assistant-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IMessageService.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.smart.assistant.entity.MessageEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+
+public interface IMessageService extends IBaseService<MessageEntity> {
+
+}
