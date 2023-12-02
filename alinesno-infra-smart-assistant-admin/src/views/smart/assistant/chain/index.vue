5f33f6903b8582baa41fd46799e852db2d38bb48
switchII <luoandon@gmail.com>
2023-12-02 20:25:02 +0800

    fix:处理js问题

--- alinesno-infra-smart-assistant-admin/src/views/smart/assistant/chain/index.vue
+++ alinesno-infra-smart-assistant-admin/src/views/smart/assistant/chain/index.vue
@@ -37,7 +37,7 @@
                 icon="Edit"
                 :disabled="single"
                 @click="handleUpdate"
-                v-hasPermi="['system:Application:edit']"
+                v-hasPermi="['system:RoleChain:edit']"
             >修改
             </el-button>
           </el-col>
@@ -48,7 +48,7 @@
                 icon="Delete"
                 :disabled="multiple"
                 @click="handleDelete"
-                v-hasPermi="['system:Application:remove']"
+                v-hasPermi="['system:RoleChain:remove']"
             >删除
             </el-button>
           </el-col>
@@ -56,7 +56,7 @@
           <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
         </el-row>
 
-        <el-table v-loading="loading" :data="ApplicationList" @selection-change="handleSelectionChange">
+        <el-table v-loading="loading" :data="RoleChainList" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="50" align="center"/>
           <el-table-column label="图标" align="center" width="80px" prop="icon" v-if="columns[0].visible">
             <template #default="scope">
@@ -102,11 +102,11 @@
             <template #default="scope">
               <el-tooltip content="修改" placement="top" v-if="scope.row.applicationId !== 1">
                 <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-                           v-hasPermi="['system:Application:edit']"></el-button>
+                           v-hasPermi="['system:RoleChain:edit']"></el-button>
               </el-tooltip>
               <el-tooltip content="删除" placement="top" v-if="scope.row.applicationId !== 1">
                 <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
-                           v-hasPermi="['system:Application:remove']"></el-button>
+                           v-hasPermi="['system:RoleChain:remove']"></el-button>
               </el-tooltip>
 
             </template>
@@ -124,7 +124,7 @@
 
     <!-- 添加或修改应用配置对话框 -->
     <el-dialog :title="title" v-model="open" width="900px" append-to-body>
-      <el-form :model="form" :rules="rules" ref="ApplicationRef" label-width="80px">
+      <el-form :model="form" :rules="rules" ref="RoleChainRef" label-width="80px">
         <el-row>
           <el-col :span="24">
             <el-form-item  label="渠道名称" prop="name">
@@ -212,22 +212,22 @@
   </div>
 </template>
 
-<script setup name="Application">
+<script setup name="RoleChain">
 import {getToken} from "@/utils/auth";
 import {
-  listApplication,
-  delApplication,
-  getApplication,
-  updateApplication,
-  addApplication,
+  listRoleChain,
+  delRoleChain,
+  getRoleChain,
+  updateRoleChain,
+  addRoleChain,
 } from "@/api/smart/assistant/chain";
 import {reactive} from "vue";
 
 const router = useRouter();
 const {proxy} = getCurrentInstance();
-// const { sys_normal_disable, sys_Application_sex } = proxy.useDict("sys_normal_disable", "sys_Application_sex");
+// const { sys_normal_disable, sys_RoleChain_sex } = proxy.useDict("sys_normal_disable", "sys_RoleChain_sex");
 
-const ApplicationList = ref([]);
+const RoleChainList = ref([]);
 const open = ref(false);
 const loading = ref(true);
 const showSearch = ref(true);
@@ -255,7 +255,7 @@ const upload = reactive({
   // 设置上传的请求头部
   headers: {Authorization: "Bearer " + getToken()},
   // 上传的地址
-  url: import.meta.env.VITE_APP_BASE_API + "/system/Application/importData"
+  url: import.meta.env.VITE_APP_BASE_API + "/system/RoleChain/importData"
 });
 // 列显隐信息
 const columns = ref([
@@ -276,7 +276,7 @@ const data = reactive({
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    ApplicationName: undefined,
+    RoleChainName: undefined,
     name: undefined,
     toolType: undefined,
     status: undefined,
@@ -304,9 +304,9 @@ const {queryParams, form, rules} = toRefs(data);
 /** 查询应用列表 */
 function getList() {
   loading.value = true;
-  listApplication(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
+  listRoleChain(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
     loading.value = false;
-    ApplicationList.value = res.rows;
+    RoleChainList.value = res.rows;
     total.value = res.total;
   });
 };
@@ -334,7 +334,7 @@ function handleDelete(row) {
   const applicationIds = row.id || ids.value;
 
   proxy.$modal.confirm('是否确认删除应用编号为"' + applicationIds + '"的数据项？').then(function () {
-    return delApplication(applicationIds);
+    return delRoleChain(applicationIds);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
@@ -360,7 +360,7 @@ function reset() {
     description: undefined,
     target: undefined,
   };
-  proxy.resetForm("ApplicationRef");
+  proxy.resetForm("RoleChainRef");
 };
 
 /** 取消按钮 */
@@ -380,7 +380,7 @@ function handleAdd() {
 function handleUpdate(row) {
   reset();
   const applicationId = row.id || ids.value;
-  getApplication(applicationId).then(response => {
+  getRoleChain(applicationId).then(response => {
     form.value = response.data;
     form.value.applicationId = applicationId;
     open.value = true;
@@ -391,16 +391,16 @@ function handleUpdate(row) {
 
 /** 提交按钮 */
 function submitForm() {
-  proxy.$refs["ApplicationRef"].validate(valid => {
+  proxy.$refs["RoleChainRef"].validate(valid => {
     if (valid) {
       if (form.value.applicationId != undefined) {
-        updateApplication(form.value).then(response => {
+        updateRoleChain(form.value).then(response => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
           getList();
         });
       } else {
-        addApplication(form.value).then(response => {
+        addRoleChain(form.value).then(response => {
           proxy.$modal.msgSuccess("新增成功");
           open.value = false;
           getList();
